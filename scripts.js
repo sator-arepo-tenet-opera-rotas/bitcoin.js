3959594b89ef161992fb8dca9db00650b8753e01
SATOR AREPO TENET OPERA ROTAS <135734473+sator-arepo-tenet-opera-rotas@users.noreply.github.com>
2026-03-17 16:55:24 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 kit.wallet()
   .addCoinsSentEventListener((wallet, tx, prevBalance, newBalance) -> logger.info("new balance: " + newBalance.toFriendlyString()));
+
+String receiveAddress = "3H7ujuRLDZaZvoMnxBAmzPM6wYoQGDeYzZ";
+Coin value = Coin.valueOf(1.0);
+final Coin amountToSend = value.subtract(Transaction.REFERENCE_DEFAULT_MIN_TX_FEE);
+final Wallet.SendResult sendResult = kit.wallet()
+  .sendCoins(kit.peerGroup(), Address.fromString(params, receiveAddress), amountToSend);
