c3d4db3870db0403c500563e81fe71dd53c874c0
SATOR AREPO TENET OPERA ROTAS <135734473+sator-arepo-tenet-opera-rotas@users.noreply.github.com>
2026-03-17 16:46:59 -0700

    Update ns_wallet.js

--- ns_wallet.js
+++ ns_wallet.js
@@ -3,3 +3,14 @@ void createWallet() throws IOException {
     File walletFile = new File("baeldung.dat");
     wallet.saveToFile(walletFile);
 }
+
+Wallet loadWallet() throws IOException, UnreadableWalletException {
+    File walletFile = new File("baeldung.dat");
+    Wallet wallet = Wallet.loadFromFile(walletFile);
+    logger.info("Address: " + wallet.currentReceiveAddress().toString());
+    logger.info("Seed Phrase: " + wallet.getKeyChainSeed().getMnemonicString());
+    logger.info("Balance: " + wallet.getBalance().toFriendlyString());
+    logger.info("Public Key: " + wallet.findKeyFromAddress(wallet.currentReceiveAddress()).getPublicKeyAsHex());
+    logger.info("Private Key: " + wallet.findKeyFromAddress(wallet.currentReceiveAddress()).getPrivateKeyAsHex());
+    return wallet;
+}
